51e0736692ce48dd386674fa2da516ce618c25bc
Andy Bursavich <abursavich@mz.com>
2019-10-05 11:53:38 -0700

    remove kadet wrapper

--- korps/types.py
+++ korps/types.py
@@ -26,32 +26,27 @@ class Object(Renderable):
         return addict.Dict()
 
 
-class Kadet:
+@typechecked
+def render(obj: Union[Dict[str, Object], Object]) -> dict:
 
-    @typechecked
-    def __init__(self, obj: Union[Dict, Object]):
-        self.obj = obj
+    def _render(value):
+        if isinstance(value, Renderable):
+            value = value.render()
+        if isinstance(value, dict):
+            base = {}
+            for k, v in value.items():
+                base[k] = _render(v)
+            return base
+        # Base64 encode bytes
+        if isinstance(value, bytes):
+            return base64.b64encode(value).decode('UTF-8')
+        # Don't convert strings into lists
+        if isinstance(value, str):
+            return value
+        try:
+            it = iter(value)
+        except TypeError:
+            return value
+        return [_render(v) for v in it]
 
-    def to_dict(self) -> dict:
-
-        def _recurse(value):
-            if isinstance(value, Renderable):
-                value = value.render()
-            if isinstance(value, dict):
-                base = {}
-                for k, v in value.items():
-                    base[k] = _recurse(v)
-                return base
-            # Base64 encode bytes
-            if isinstance(value, bytes):
-                return base64.b64encode(value).decode('UTF-8')
-            # Don't convert strings into lists
-            if isinstance(value, str):
-                return value
-            try:
-                it = iter(value)
-            except TypeError:
-                return value
-            return [_recurse(v) for v in it]
-
-        return _recurse(self.obj)
+    return _render(obj)
